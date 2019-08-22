10b5797e21c4639982c03d90b8f02d2b35372e50
Maurizio Turatti <maurizio@softinstigate.com>
2019-08-22 10:39:09 +0200

    Update push.sh

--- bin/push.sh
+++ bin/push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-    docker push softinstigate/serverless:latest;
-    docker push softinstigate/serverless:"$RELEASE";
-fi
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+docker push softinstigate/serverless:latest;
+docker push softinstigate/serverless:"$RELEASE";
